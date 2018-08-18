c5f7bf00efeff0569e78c7b644f1951358720f61
Jessica Hines <jessicahines717@gmail.com>
2018-08-18 18:43:06 -0500

    MAKE IT WERK

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,24 @@
 class HomeController < ApplicationController
   def index
+    if params[:zipcode].present?
+      redirect_to(url_for(
+        :controller => 'locations',
+        :action => 'show',
+        :id => Location.find_by(zipcode: params[:zipcode].to_i).id
+      ))
+    elsif params[:lat] && params[:lng]
+      redirect_to(url_for(
+        :controller => 'locations',
+        :action => 'show',
+        :id => Location.find_by(zipcode: fetch_zipcode.to_i).id
+      ))
+    else
+    end
+  end
+
+  def fetch_zipcode
+    lat = params[:lat]
+    lng = params[:lng]
+    Geocoder.search([lat, lng]).first.postal_code
   end
 end
