a8e8f78c88849fc6077cd8d2b239007a2ee7b1a8
Nick Anderson <tetramputechture@gmail.com>
2018-08-18 17:15:34 -0500

    add score adj, validation

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,33 @@
 class Location < ApplicationRecord
+  validates_inclusion_of :score, in: 1..100
   validates :zipcode, :assault_count, :shooting_count, 
             :rape_count, :theft_count, :burglary_count, 
             :robbery_count, presence: true
+  
+  def score_phrase
+    case score
+    when 1..10
+      'Very very much not safe'
+    when 11..20
+      'Very much not safe'
+    when 21..30
+      'Much not safe'
+    when 31..40
+      'Not safe'
+    when 41..50
+      'Use caution'
+    when 51.60
+      'Be cautious'
+    when 61..70
+      'Eh'
+    when 71..80
+      'Hide your wallet'
+    when 81..90
+      'Pretty safe'
+    when 91..100
+      'You fuckin did it'
+    else
+      'Invalid score'
+    end
+  end
 end
