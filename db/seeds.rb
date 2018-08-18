ad37b1ecdda2b2f9b6d193d1d0e0b5fc6067e7a2
Jessica Hines <jessicahines717@gmail.com>
2018-08-18 13:13:45 -0500

    a single seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+Location.create(zipcode: 35203, score: 100)
