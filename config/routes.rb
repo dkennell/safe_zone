efc04051474d7efaec311908f46b05d0ce4af996
Nick Anderson <tetramputechture@gmail.com>
2018-08-18 13:34:20 -0500

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
 
-  get 'locations/show'
-
+  get '/locations/:id', to: 'locations#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
