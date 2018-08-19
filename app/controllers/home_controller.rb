4ec77af5248e390625a6f8765caa6fa835e6f03c
Daniel Latham <daniellatham11@gmail.com>
2018-08-19 08:00:48 -0500

    Add routing errors on failed lookup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,20 @@
 class HomeController < ApplicationController
   def index
     if params[:zipcode].present?
+      location = Location.find_by(zipcode: params[:zipcode].to_i)
+      raise ActionController::RoutingError.new('Not Found') unless location.present?
       redirect_to(url_for(
         :controller => 'locations',
         :action => 'show',
-        :id => Location.find_by(zipcode: params[:zipcode].to_i).id
+        :id => location.id
       ))
     elsif params[:lat] && params[:lng]
+      location = Location.find_by(zipcode: fetch_zipcode.to_i)
+      raise ActionController::RoutingError.new('Not Found') unless location.present?
       redirect_to(url_for(
         :controller => 'locations',
         :action => 'show',
-        :id => Location.find_by(zipcode: fetch_zipcode.to_i).id
+        :id => location.id
       ))
     else
     end
