5950d04f5247edc937bcaea8cd4673e268e51574
Jessica Hines <jessicahines717@gmail.com>
2018-08-18 20:10:48 -0500

    LET THERE BE COLORS

--- app/models/location.rb
+++ app/models/location.rb
@@ -30,4 +30,31 @@ class Location < ApplicationRecord
       '[invalid score]'
     end
   end
+
+  def score_color
+    case score
+    when 1..10
+      '#FF3232'
+    when 11..20
+      '#FF4130'
+    when 21..30
+      '#FF7830'
+    when 31..40
+      '#FF8630'
+    when 41..50
+      '#FF9730'
+    when 51.60
+      '#F4B22E'
+    when 61..70
+      '#F7C12E'
+    when 71..80
+      '#E5CF2B'
+    when 81..90
+      '#8FC46A'
+    when 91..100
+      '#5AA863'
+    else
+      '#FFFFFF'
+    end
+  end
 end
